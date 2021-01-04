7fcd8f8b7447b998bcc9d74d9984de095de43354
Leja Aprianza <72089263+lejaaprianza@users.noreply.github.com>
2021-01-04 13:18:01 +0700

    Add files via upload

---
+++ components/RefsDemo.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+class RefsDemo extends Component {
+    constructor(props) {
+        super(props)
+        
+        this.inputRef = React.createRef()
+        this.cbRef = null
+        this.setCBRef = (element) =>{
+            this.cbRef = element
+        }
+    }
+    
+    componentDidMount(){
+        if(this,this.cbRef){
+            this.cbRef.focus()
+        }
+        // this.inputRef.current.focus()
+        // console.log(this.inputRef)
+    }
+
+    clickHandler =() =>{
+        alert(this.inputRef.current.value)
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" ref={this.inputRef}/>
+                <input type="text" ref={this.setCBRef}/>
+                <button onClick={this.clickHandler}>click</button>
+            </div>
+        )
+    }
+}
+
+export default RefsDemo
+
