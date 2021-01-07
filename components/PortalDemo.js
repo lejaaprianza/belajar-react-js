b58e29d9d3da198f8a132158abd23f64a2050206
Leja Aprianza <72089263+lejaaprianza@users.noreply.github.com>
2021-01-07 12:03:27 +0700

    Add files via upload

---
+++ components/PortalDemo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function PortalDemo() {
+    return ReactDOM.createPortal(
+        <div>
+            <h1>
+                Portal demo
+            </h1>
+            
+        </div>,
+        document.getElementById('portal-root')
+    )
+}
+
+export default PortalDemo
